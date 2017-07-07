ba1e76f62e09d38dd603595adb7a5d92c383714a
Christine ong <cosy.ily@gmail.com>
2017-07-07 19:01:21 +0800

    no message

--- services/wit.js
+++ services/wit.js
@@ -135,19 +135,7 @@ var getWeather = function (location) {
 	})
 }
 
-var getSched = function (prog) {
-	return new Promise(function (resolve, reject) {
-		var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22'+ location +'%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
-		request(url, function (error, response, body) {
-		    if (!error && response.statusCode == 200) {
-		    	var jsonData = JSON.parse(body)
-		    	var forecast = jsonData.query.results.channel.item.forecast[0].text
-		      console.log('WEATHER API SAYS....', jsonData.query.results.channel.item.forecast[0].text)
-		      return forecast
-		    }
-			})
-	})
-}
+
 
 // CHECK IF URL IS AN IMAGE FILE
 var checkURL = function (url) {
